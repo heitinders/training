f844c7e8f8478d314742037112c6c59ee3031ca0
“Reet”kaur86 <reetkaur86@yahoo.com>
2022-03-25 13:40:20 -0400

    3/25/2022

---
+++ src/components/Into/Intro.js
@@ -0,0 +1,23 @@
+import "./Intro.css";
+
+const Intro = () => (
+  <div className="container">
+    <h3>Hello World</h3>
+  </div>
+);
+
+export default Intro;
+
+/*
+ this is called component in react
+ this is a functional component 
+ jsx 
+ funciton compoennt
+ class component
+ package.json
+ npm
+ node js
+ virtual dom 
+ react 
+
+*/
