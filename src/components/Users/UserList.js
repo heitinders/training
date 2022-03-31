3ca6e961d97160bb505e6042fe8f32bb08b26536
“Reet”kaur86 <heitinder.js@gmail.com>
2022-03-31 14:40:04 -0400

    changed userList to have a heading

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -10,46 +10,49 @@ import Button from "@mui/material/Button";
 
 const UserList = (props) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">ID</TableCell>
-            <TableCell align="right">First Name</TableCell>
-            <TableCell align="right">Last Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Avatar</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props?.listItems?.map((item, index) => (
-            <TableRow
-              key={index}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="right">{item.id}</TableCell>
-              <TableCell align="right">{item.first_name}</TableCell>
-              <TableCell align="right">{item.last_name}</TableCell>
-              <TableCell align="right">{item.email}</TableCell>
-              <TableCell align="right">
-                <img src={item.avatar}></img>
-              </TableCell>
-              <TableCell align="right">
-                <Stack direction="row" spacing={2}>
-                  <Button variant="outlined" color="success">
-                    EDIT
-                  </Button>
-                  <Button variant="contained" color="error">
-                    DELETE
-                  </Button>
-                </Stack>
-              </TableCell>
+    <>
+      <h3> this change is only for git</h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">ID</TableCell>
+              <TableCell align="right">First Name</TableCell>
+              <TableCell align="right">Last Name</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Avatar</TableCell>
+              <TableCell align="right">Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {props?.listItems?.map((item, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="right">{item.id}</TableCell>
+                <TableCell align="right">{item.first_name}</TableCell>
+                <TableCell align="right">{item.last_name}</TableCell>
+                <TableCell align="right">{item.email}</TableCell>
+                <TableCell align="right">
+                  <img src={item.avatar}></img>
+                </TableCell>
+                <TableCell align="right">
+                  <Stack direction="row" spacing={2}>
+                    <Button variant="outlined" color="success">
+                      EDIT
+                    </Button>
+                    <Button variant="contained" color="error">
+                      DELETE
+                    </Button>
+                  </Stack>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
